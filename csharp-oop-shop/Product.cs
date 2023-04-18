ac4dce98ab8ea2795dffd758e4616e8eeb5c36ba
Khodazone <khodazone@gmail.com>
2023-04-18 17:43:18 +0200

    end program

---
+++ csharp-oop-shop/Product.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpoopshop
+{
+    public class Product
+    {
+        public string Code { get; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public byte Iva { get; set; }
+
+        public Product(string name, string description, double price, byte iva)
+        {
+            Code = CreateCode();
+            Name = name;
+            Description = description;
+            Price = price;
+            Iva = iva;
+        }
+
+        public static string CreateCode()
+        {
+            var code = new Random().Next(0, 1000);
+
+            return code.ToString().PadLeft(8, '0');
+        }
+
+        public double PriceIva()
+        {
+            var price_iva = (Price / (100 * 100)) * ((Iva + 100) * 100);
+
+            return price_iva;
+        }
+
+        public string NameCode()
+        {
+            var result = $"{Code} | {Name}";
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            var nl = Environment.NewLine;
+
+            var productWrite =
+                $"Nome prodotto: {Name}" + nl
+                + $"Descrizione prodotto: {Description}" + nl
+                + $"Prezzo prodotto: {Price} euro" + nl
+                + $"IVA: {Iva}%" + nl
+                + $"Prezzo + IVA: {PriceIva()} euro";
+
+            return productWrite;
+        }
+    }
+}
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Creare la classe Prodotto che gestisce i prodotti dello shop.
+ * Un prodotto è caratterizzato da:
+ * - codice (numero intero)
+ * - nome
+ * - descrizione
+ * - prezzo
+ * - iva
+ * Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter (o eventualmente le properties) ed eventuali altri metodi di “utilità” per fare in modo che:
+ * Alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+ * Il codice prodotto sia accessibile solo in lettura
+ * Gli altri attributi siano accessibili sia in lettura che in scrittura
+ * Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
+ * Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
+ */
+
+using csharpoopshop;
+
+Console.WriteLine("Numero prodotti:");
+int productsNum = Convert.ToInt32(Console.ReadLine());
+
+Product[] products = new Product[productsNum];
+
+for (int i = 0; i < productsNum; i++)
+{
+    Console.WriteLine($"Inserire nome prodotto {i + 1}:");
+    string name = Console.ReadLine();
+
+    Console.WriteLine($"Inserire descrizione prodotto {i + 1}:");
+    string description = Console.ReadLine();
+
+    Console.WriteLine($"Inserire prezzo prodotto {i + 1}:");
+    double price = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine($"Inserire iva prodotto {i + 1}:");
+    byte iva = (byte)Convert.ToInt32(Console.ReadLine());
+
+
+    Product _object = new Product(name, description, price, iva);
+
+    products[i] = _object;
+}
+
+
+Console.WriteLine("LISTA PRODOTTI:");
+
+for (int i = 0; i < products.Length; i++)
+{
+    Console.WriteLine(products[i].ToString());
+}
